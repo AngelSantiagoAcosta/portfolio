b364fd4f12880109b422f70ff1ba7b2d48e3ff90
Angel Acosta <aswacosta@gmail.com>
2022-03-08 10:19:08 -0600

    adding endpoints, updating index.hmtl

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+#portfolio page route
+@app.route("/portfolio", methods= ['GET', 'POST'])
+def portfolio():
+	return render_template('portfolio.html')
+
+
+#contact me page route
+@app.route("/contact")
+def contact():
+	return render_template('contact.html')
     
 if __name__ == "__main__":
     app.run(debug=True)
